e76b02de2c163cc5a1d3c43948079787bb2683e0
Peter Stromquist <peter@twistedstream.com>
2016-11-14 21:51:52 -0600

    Replace session management with encrypted cookie

--- server.js
+++ server.js
@@ -6,10 +6,9 @@ const uuid = require('node-uuid');
 const cookieParser = require('cookie-parser');
 const request = require('request');
 const jwt = require('jsonwebtoken');
+const sessions = require('client-sessions');
 
 const app = new Express();
-app.use(cookieParser());
-app.use(morgan('dev'));
 module.exports = app;
 
 // returns the current config container
@@ -89,83 +88,80 @@ function getService () {
   };
 }
 
-//TODO: replace session management with encrypted cookie
-
-const SESSION_COOKIE_NAME = 'CAS_SESSION';
-
-// saves the current session to the session cookie
-function saveSession (req, res) {
-  // unencrypted for now
-  res.cookie(SESSION_COOKIE_NAME, JSON.stringify(req.session), {
-    secure: config(req).SECURE_COOKIE === 'true',
-    httpOnly: true });
-}
-
-// load session at beginning of request
 app.use((req, res, next) => {
-  const sessionCookie = req.cookies[SESSION_COOKIE_NAME];
-  req.session = (sessionCookie && JSON.parse(sessionCookie)) || {};
+  const router = new Express.Router();
 
-  next();
-});
+  router.use(cookieParser());
+  router.use(morgan('dev'));
 
-// CAS login endpoint
-app.get('/login', requireParams(['service']), getService(), (req, res) => {
-  // generate session
-  req.session.ticket = uuid.v4();
-  req.session.serviceUrl = req.query.service;
-  saveSession(req, res);
+  // configure encrypted session
+  router.use(sessions({
+    cookieName: 'cas-session',
+    requestKey: 'session',
+    secret: config(req).SESSION_SECRET,
+    duration: 24 * 60 * 60 * 1000,
+    activeDuration: 1000 * 60 * 5
+  }));
 
-  // perform OIDC Authorizaation Code Flow with Auth0
-  const query = querystring.stringify({
-    client_id: req.service.client_id,
-    response_type: 'code',
-    scope: 'openid profile',
-    redirect_uri: config(req).AUTH0_REDIRECT_URI,
-    connection: config(req).AUTH0_CONNECTION,
-    state: req.session.ticket
-  });
-  res.redirect(`https://${config(req).AUTH0_DOMAIN}/authorize?${query}`);
-});
+  // CAS login endpoint
+  router.get('/login', requireParams(['service']), getService(), (req, res) => {
+    // generate session
+    req.session.ticket = uuid.v4();
+    req.session.serviceUrl = req.query.service;
 
-// Auth0 Authorization Code Flow callback endpoint
-app.get('/callback', requireParams(['code', 'state']), (req, res) => {
-  // validate session
-  if (req.session.ticket !== req.query.state) return res.status(400).send(`Invalid session`);
-
-  // store code in session
-  req.session.code = req.query.code;
-  saveSession(req, res);
-
-  res.redirect(`${req.session.serviceUrl}?ticket=${req.session.ticket}`);
-});
-
-// CAS validate endpoint
-app.get('/p3/serviceValidate', requireParams(['service', 'ticket']), getService(), (req, res) => {
-  // validate ticket
-  if (req.session.ticket !== req.query.ticket) return res.status(400).send(`Invalid ticket`);
-
-  // perform OAuth2 code/token exchange with Auth0
-  request.post({
-    url: `https://${config(req).AUTH0_DOMAIN}/oauth/token`,
-    json: {
-      code: req.session.code,
+    // perform OIDC Authorizaation Code Flow with Auth0
+    const query = querystring.stringify({
       client_id: req.service.client_id,
-      client_secret: req.service.client_secret,
-      grant_type: 'authorization_code',
-      redirect_uri: config(req).AUTH0_REDIRECT_URI
-    }
-  }, (err, response, body) => {
-    if (err) throw err;
-    if (response.statusCode != 200) return res.status(400).send(`IDP returned a non-successful response: ${body}`);
+      response_type: 'code',
+      scope: 'openid profile',
+      redirect_uri: config(req).AUTH0_REDIRECT_URI,
+      connection: config(req).AUTH0_CONNECTION,
+      state: req.session.ticket
+    });
+    res.redirect(`https://${config(req).AUTH0_DOMAIN}/authorize?${query}`);
+  });
 
-    //TODO: replace with RS256 verification
+  // Auth0 Authorization Code Flow callback endpoint
+  router.get('/callback', requireParams(['code', 'state']), (req, res) => {
+    // validate session
+    if (req.session.ticket !== req.query.state) return res.status(400).send(`Invalid session`);
 
-    // validate the id_token and return its payload in CAS format
-    jwt.verify(body.id_token, new Buffer(req.service.client_secret, 'base64'), (err, payload) => {
+    // store code in session
+    req.session.code = req.query.code;
+
+    res.redirect(`${req.session.serviceUrl}?ticket=${req.session.ticket}`);
+  });
+
+  // CAS validate endpoint
+  router.get('/p3/serviceValidate', requireParams(['service', 'ticket']), getService(), (req, res) => {
+    // validate ticket
+    if (req.session.ticket !== req.query.ticket) return res.status(400).send(`Invalid ticket`);
+
+    // perform OAuth2 code/token exchange with Auth0
+    request.post({
+      url: `https://${config(req).AUTH0_DOMAIN}/oauth/token`,
+      json: {
+        code: req.session.code,
+        client_id: req.service.client_id,
+        client_secret: req.service.client_secret,
+        grant_type: 'authorization_code',
+        redirect_uri: config(req).AUTH0_REDIRECT_URI
+      }
+    }, (err, response, body) => {
       if (err) throw err;
+      if (response.statusCode != 200) return res.status(400).send(`IDP returned a non-successful response: ${body}`);
 
-      res.send(payload);
+      //TODO: replace with RS256 verification
+
+      // validate the id_token and return its payload in CAS format
+      jwt.verify(body.id_token, new Buffer(req.service.client_secret, 'base64'), (err, payload) => {
+        if (err) throw err;
+
+        res.send(payload);
+      });
     });
   });
+
+  app.use('/', router);
+  next();
 });
